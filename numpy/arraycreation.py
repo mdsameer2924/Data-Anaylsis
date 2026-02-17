8a3acccc9811b05da9aebb6994809f0c7238d392
mdsameer2924 <mdsameer2681@gmail.com>
2026-02-17 21:18:19 +0530

    Stuck and forgot how to create an array in numpy exactly

---
+++ numpy/arraycreation.py
@@ -0,0 +1,8 @@
+''' THis is practice set to make a different type of array
+🔹 1-D Array 
+🔹 2-D Array
+🔹 3-D Array
+'''
+import numpy as np
+onedarray=np.onedarray([1,2,3,4,5]) # how to create an array in numpy ? stuck
+print(onedarray)
