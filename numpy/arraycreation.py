58b05a7fc62752defef604e0760d45b8faf9b63f
mdsameer2924 <mdsameer2681@gmail.com>
2026-02-17 21:47:14 +0530

    3d array created layer/depth introduce beyond rows and columns

--- numpy/arraycreation.py
+++ numpy/arraycreation.py
@@ -1,8 +1,27 @@
 ''' THis is practice set to make a different type of array
-🔹 1-D Array 
-🔹 2-D Array
+🔹 1-D Array ✔️
+🔹 2-D Array ✔️
 🔹 3-D Array
 '''
 import numpy as np
 onedarray=np.array([1,2,3,4,5]) # how to create an array in numpy ? stuck
 print(onedarray)
+
+twodarray=np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9] 
+                   ])
+print(twodarray)
+
+## 3D array # shape would be (4,2,3)
+''' Where 4 = layer/depth
+2 = rows
+3 = columns
+'''
+threedarray=np.array([
+                      [[1,3,4],[2,5,6]],
+                      [[3,4,9],[23,53,12]],
+                      [[9,7,8],[2,12,2]],
+                      [[2,6,9],[4,5,6]]
+                      ]) 
+print(threedarray.shape)
